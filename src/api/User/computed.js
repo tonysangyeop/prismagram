22e6252e7c74f1e016ad53d5d83bcfaa77ce0a71
sanghae <sy_lee@wenwo.co.kr>
2019-05-26 19:17:28 +0900

    bug

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -1,53 +1,34 @@
 import { prisma } from "../../../generated/prisma-client";
 
 export default {
-    User: {
-        fullName: parent => {
-            return `${parent.firstName} ${parent.lastName}`;
-        },
-        isFollowing: async(parent, _, { request }) => {
-            const { user } = request;
-            const { id: parentId } = parent;
-            try {
-                return prisma.$exists.user({
-                    AND: [
-                        {
-                            id:parentId
-                        }, {
-                            followers_some: {
-                                id: user.id
-                            }
-                        }
-                    ] 
-                });
-            } catch {
-                return false;
-            }
-        },
-        isSelf: (parent, _, {request}) => {
-            const { user } = request;
-            const { id: parentId } = parent;
-            return user.id === parentId
-        }
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
     },
-    Post: {
-        isLiked: (parent, _, { request }) => {
-            const { user } = request;
-            const { id } = parent;
-            return prisma.$exists.like({
-                AND:[
-                    {
-                        user: {
-                            id: user.id
-                        }
-                    },
-                    {
-                        post: {
-                            id
-                        }
-                    }
-                ]
-            })   
-        }
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [
+            {
+              id: user.id
+            },
+            {
+              following_some: {
+                id: parentId
+              }
+            }
+          ]
+        });
+      } catch {
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
     }
-}
+  }
+};
