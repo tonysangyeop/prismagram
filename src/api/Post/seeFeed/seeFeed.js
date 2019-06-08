0bec8eab5aaecac061c7c33d533c85c459f917de
sanghae <sy_lee@wenwo.co.kr>
2019-06-08 15:55:49 +0900

    graphql backend

--- src/api/Post/seeFeed/seeFeed.js
+++ src/api/Post/seeFeed/seeFeed.js
@@ -6,8 +6,12 @@ export default {
            isAuthenticated(request);
            const { user } = request;
            const following = await prisma.user({ id : user.id}).following();
-           console.log(following);
-           return [];
+           return prisma.posts({ where : {
+               user: {
+                   id_in: [...following.map(user => user.id), user.id]
+               }
+            }
+        });
         }
     }
 }
